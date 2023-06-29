855cb9606caae6db3d9f08cdedde56e828df3d37
sourav-itobuz <sourav.dutta@itobuz.com>
2023-06-29 15:08:43 +0530

    final commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let list = [];
 let flag=0;
+let increase;
 function add() {
   document.getElementById("result").innerHTML = "";
   let task = document.getElementById("name").value.trim();
@@ -11,10 +12,10 @@ function add() {
   };
   list.push(listitem);
   for (i = 0; i < list.length; i++) {
-    document.getElementById("result").innerHTML += `<div class="d-flex justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
-        <h3>Name:${list[i].name}</h4>
-                 <h3>Amount:${list[i].amount}</h3>
-                 <button class="btn btn-dark text-white rounded-circle fw-bolder" onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
+    document.getElementById("result").innerHTML += `<div class="d-flex flex-column flex-md-row justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
+        <h3>Name: ${list[i].name}</h4>
+                 <h3>Amount: ${list[i].amount}</h3>
+                 <button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center" onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
                  </div>`;
   }
   document.getElementById("name").value="";
@@ -23,14 +24,23 @@ function add() {
 }
 
 function increment(i){
+  document.getElementById("result").innerHTML = `<div class="d-flex gap-2"><input type="number" id="addValue" placeholder = "Amount">
+  <button class="inputButton btn btn-dark text-white fw-bolder align-self-center" onclick="update(${i})">submit</button></div>`;
+  increase = document.getElementById("addValue");
+}
+
+function update(i){
+    list[i].amount = Number(increase.value) + Number(list[i].amount);
     document.getElementById("result").innerHTML = "";
-    let increase=prompt("Enter amount to increase:");
-    list[i].amount = Number(increase) + Number(list[i].amount);
     for (i = 0; i < list.length; i++) {
-        document.getElementById("result").innerHTML += `<div class="d-flex justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
-        <h3>Name:${list[i].name}</h3>
-                 <h3>Amount:${list[i].amount}</h3>
-                 <button class="btn btn-dark text-white rounded-circle fw-bolder" onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
+        document.getElementById("result").innerHTML += `<div class="d-flex flex-column flex-md-row justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
+        <h3>Name: ${list[i].name}</h3>
+                 <h3>Amount: ${list[i].amount}</h3>
+                 <button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center" onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
                  </div>`;
       }
+}
+
+function addButton(event) {
+  if (event.key === "Enter") add();
 }
