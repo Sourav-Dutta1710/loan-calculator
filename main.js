6dd8f293399540c76d7c277b60f08b18cfd5a5d7
sourav-itobuz <sourav.dutta@itobuz.com>
2023-07-03 18:49:12 +0530

    third commit

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 let list = [];
-let flag = 0;
-let increase;
+
+function showListItem() {
+  document.getElementById( "result" ).innerHTML = "";
+  for (i = 0; i < list.length; i++) {
+  document.getElementById(
+    "result"
+  ).innerHTML += `<div class="d-flex flex-column flex-md-row justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
+    <h3>Name: ${list[i].name}</h4>
+             <h3>Amount: <span class = "current-amount">${list[i].amount}</span></h3>
+             <div class="d-flex gap-2 ">
+             <input type="number"  placeholder = "Amount" class="hidden-item addValue" id = "submit">
+               <button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center" 
+             onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
+             </div>`;
+}
+}
+
+
 function add() {
-  document.getElementById("result").innerHTML = "";
   let task = document.getElementById("name").value.trim();
   if (task === "") alert("Name Field Blank");
   else {
@@ -11,40 +26,22 @@ function add() {
       amount: document.getElementById("amount").value,
     };
     list.push(listItem);
-    for (i = 0; i < list.length; i++) {
-      document.getElementById(
-        "result"
-      ).innerHTML += `<div class="d-flex flex-column flex-md-row justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
-        <h3>Name: ${list[i].name}</h4>
-                 <h3>Amount: ${list[i].amount}</h3>
-                 <button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center" onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
-                 </div>`;
-    }
+      showListItem();
     document.getElementById("name").value = "";
     document.getElementById("amount").value = "";
   }
 }
 
 function increment(i) {
-  document.getElementById(
-    "result"
-  ).innerHTML = `<div class="d-flex gap-2"><input type="number" id="addValue" placeholder = "Amount">
-  <button class="inputButton btn btn-dark text-white fw-bolder align-self-center" onclick="update(${i})">submit</button></div>`;
-  increase = document.getElementById("addValue");
-}
-
-function update(i) {
-  list[i].amount = Number(increase.value) + Number(list[i].amount);
-  document.getElementById("result").innerHTML = "";
-  for (i = 0; i < list.length; i++) {
-    document.getElementById(
-      "result"
-    ).innerHTML += `<div class="d-flex flex-column flex-md-row justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
-        <h3>Name: ${list[i].name}</h3>
-                 <h3>Amount: ${list[i].amount}</h3>
-                 <button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center" onclick="increment(${i})"><i class="fa-solid fa-plus"></i></button>
-                 </div>`;
-  }
+  document.getElementsByClassName("addValue")[i].classList.toggle("hidden-item");
+  
+  document.getElementsByClassName("addValue")[i].addEventListener("keydown", (e)=>{
+    if(e.key === "Enter"){
+      list[i].amount =  (Number(document.getElementsByClassName("addValue")[i].value)) + (Number(document.getElementsByClassName("current-amount")[i].innerHTML));
+      document.getElementsByClassName("addValue")[i].classList.add("hidden-item");
+      showListItem();
+    }
+  })
 }
 
 function addButton(event) {
