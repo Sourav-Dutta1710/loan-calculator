3920a0d0309b545ba6498ea4d96f981c50a49577
sourav-itobuz <sourav.dutta@itobuz.com>
2023-07-19 19:36:18 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const list = JSON.parse(localStorage.getItem("list")) || [];
 const result = document.getElementById("result");
+const taskName = document.getElementById("name");
+const taskAmount = document.getElementById("amount");
 
 function showListItem(flag) {
   result.innerHTML = "";
@@ -7,77 +9,51 @@ function showListItem(flag) {
     document.getElementById(
       "result"
     ).innerHTML += `<div class="d-flex flex-column flex-md-row justify-content-center bg-white rounded-5 p-3 mb-4 gap-3">
-    <h3>Name: ${list[i].name}</h4>
-             <h3>Amount: <span class = "current-amount">${list[i].amount}</span></h3>
-             <div class="d-flex gap-2 ">
-             <input type="number"  placeholder = "Amount" class="hidden-item addValue" id = "submit">
-               <span class = "addButton"><button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center"
-             onclick="increment(${list[i].id},1)"><img src="./images/plus-solid.svg"/></button></span>
-             <span class = "minusButton"><button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center"
-             onclick="increment(${list[i].id},0)"><img src="./images/minus-solid.svg"/></button></span>
-             <span class = "deleteButton"><button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center"
-             onclick="deletePerson(${list[i].id})"><img src="./images/dumpster-solid.svg"/></button></span>
-             </div>`;
-             if(flag === 1)
-                localStorage.setItem("list",JSON.stringify(list))           
+      <h3>Name: ${list[i].name}</h3>
+      <h3>Amount: <span class = "current-amount">${list[i].amount}</span></h3>
+      <div class="d-flex gap-2 ">
+      <span class = "addButton"><button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center"
+      onclick="editTask(${list[i].id})"><img src="./images/pen-solid.svg"/></button></span>
+      <span class = "deleteButton"><button class="inputButton btn btn-dark text-white rounded-circle fw-bolder align-self-center"
+      onclick="deletePerson(${list[i].id})"><img src="./images/dumpster-solid.svg"/></button></span>
+      </div>`;
+      if(flag === 1)
+        localStorage.setItem("list",JSON.stringify(list))           
   }
 }
 
 function addTask() {
-  const taskName = document.getElementById("name").value;
-  const taskAmount = document.getElementById("amount").value;
-  let flag = 0;
-  const task = taskName.trim();
-  if (!task || !taskAmount) 
+  const task = taskName.value.trim();
+  if (!task || !taskAmount.value) 
     alert("name or amount blank");
-  else if (taskAmount < 1)
+  else if (taskAmount.value < 1)
     alert("enter valid amount");
    else {
     const listItem = {
       id:new Date().getTime(),
-      name: taskName,
-      amount: taskAmount,
+      name: taskName.value,
+      amount: taskAmount.value,
     };
-    for (i = 0; i < list.length; i++) {
-      if (list[i].name === listItem.name) {
-        increment(i,0);
-        flag = 1;
-      }
-    }
-    if (flag === 0) {
-      list.push(listItem);
-      showListItem(1);
-    }
+    for (i = 0; i < list.length; i++) 
+      if (list[i].name === listItem.name) deletePerson(list[i].id);
+    list.push(listItem);
+    showListItem(1);
   }
-  document.getElementById("name").value = "";
+  if(taskAmount.value > 0)
+     document.getElementById("name").value = "";
   document.getElementById("amount").value = "";
 }
 
-function increment(id, j) {
-  for(k=0;k<list.length;k++)
+function editTask(id) {
+   for(k=0;k<list.length;k++)
     if(list[k].id === id)i=k;
-  const addValue = document.getElementsByClassName("addValue")[i];
-  let currentAmount = document.getElementsByClassName("current-amount")[i];
-  addValue.classList.remove("hidden-item");
-  document.getElementsByClassName("minusButton")[i].classList.add("hidden-item");
-  document.getElementsByClassName("addButton")[i].classList.add("hidden-item");
-  document.getElementsByClassName("deleteButton")[i].classList.add("hidden-item");
-  addValue.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        if(addValue.value <= 0)
-          alert("enter valid amount");
-        else if (j === 1)
-          list[i].amount =
-            Number(addValue.value) +
-            Number(currentAmount.innerHTML);
-        else
-          list[i].amount =
-            -Number(addValue.value) +
-            Number(currentAmount.innerHTML);
-        addValue.classList.add("hidden-item");
-        showListItem(1);
-      }
-    });
+  document.getElementById("name").value = list[i].name;
+  document.getElementById("amount").value = list[i].amount;
+  list.map((person) => {
+    if(person.id === id)
+      person.amount = Number(taskAmount.value);
+  });
+  showListItem(0);
 }
 
 function addButton(event) {
@@ -97,5 +73,4 @@ function deleteAll(){
   localStorage.clear();
   showListItem(0);
 }
-
 showListItem(0);
